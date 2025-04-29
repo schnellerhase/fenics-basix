74af09c42d1384c4e3a76ee2bb3a282f0aad15e7
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-04-30 00:42:00 +0200

    Fix performance-unnecessary-value-param

--- cpp/basix/e-lagrange.cpp
+++ cpp/basix/e-lagrange.cpp
@@ -437,7 +437,7 @@ template <std::floating_point T>
 FiniteElement<T>
 basix::element::create_lagrange(cell::type celltype, int degree,
                                 lagrange_variant variant, bool discontinuous,
-                                std::vector<int> dof_ordering)
+                                const std::vector<int>& dof_ordering)
 {
   if (celltype == cell::type::point)
   {
@@ -765,10 +765,10 @@ FiniteElement<T> basix::element::create_iso(cell::type celltype, int degree,
 //-----------------------------------------------------------------------------
 template FiniteElement<float> element::create_lagrange(cell::type, int,
                                                        lagrange_variant, bool,
-                                                       std::vector<int>);
-template FiniteElement<double> element::create_lagrange(cell::type, int,
-                                                        lagrange_variant, bool,
-                                                        std::vector<int>);
+                                                       const std::vector<int>&);
+template FiniteElement<double>
+element::create_lagrange(cell::type, int, lagrange_variant, bool,
+                         const std::vector<int>&);
 template FiniteElement<float> element::create_iso(cell::type, int,
                                                   lagrange_variant, bool);
 template FiniteElement<double> element::create_iso(cell::type, int,
--- python/wrapper.cpp
+++ python/wrapper.cpp
@@ -90,7 +90,7 @@ void declare_float(nb::module_& m, const std::string& type)
   nb::class_<FiniteElement<T>>(m, name.c_str())
       .def("tabulate",
            [](const FiniteElement<T>& self, int n,
-              nb::ndarray<const T, nb::ndim<2>, nb::c_contig> x)
+              const nb::ndarray<const T, nb::ndim<2>, nb::c_contig>& x)
            {
              mdspan_t<const T, 2> _x(x.data(), x.shape(0), x.shape(1));
              return as_nbarrayp(self.tabulate(n, _x));
@@ -131,10 +131,10 @@ void declare_float(nb::module_& m, const std::string& type)
            })
       .def("push_forward",
            [](const FiniteElement<T>& self,
-              nb::ndarray<const T, nb::ndim<3>, nb::c_contig> U,
-              nb::ndarray<const T, nb::ndim<3>, nb::c_contig> J,
-              nb::ndarray<const T, nb::ndim<1>, nb::c_contig> detJ,
-              nb::ndarray<const T, nb::ndim<3>, nb::c_contig> K)
+            const nb::ndarray<const T, nb::ndim<3>, nb::c_contig>& U,
+            const nb::ndarray<const T, nb::ndim<3>, nb::c_contig>& J,
+            const nb::ndarray<const T, nb::ndim<1>, nb::c_contig>& detJ,
+            const nb::ndarray<const T, nb::ndim<3>, nb::c_contig>& K)
            {
              auto u = self.push_forward(
                  mdspan_t<const T, 3>(U.data(), U.shape(0), U.shape(1),
@@ -148,10 +148,10 @@ void declare_float(nb::module_& m, const std::string& type)
            })
       .def("pull_back",
            [](const FiniteElement<T>& self,
-              nb::ndarray<const T, nb::ndim<3>, nb::c_contig> u,
-              nb::ndarray<const T, nb::ndim<3>, nb::c_contig> J,
-              nb::ndarray<const T, nb::ndim<1>, nb::c_contig> detJ,
-              nb::ndarray<const T, nb::ndim<3>, nb::c_contig> K)
+              const nb::ndarray<const T, nb::ndim<3>, nb::c_contig>& u,
+              const nb::ndarray<const T, nb::ndim<3>, nb::c_contig>& J,
+              const nb::ndarray<const T, nb::ndim<1>, nb::c_contig>& detJ,
+              const nb::ndarray<const T, nb::ndim<3>, nb::c_contig>& K)
            {
              auto U = self.pull_back(
                  mdspan_t<const T, 3>(u.data(), u.shape(0), u.shape(1),
@@ -164,18 +164,18 @@ void declare_float(nb::module_& m, const std::string& type)
              return as_nbarrayp(std::move(U));
            })
       .def("T_apply", [](const FiniteElement<T>& self,
-                         nb::ndarray<T, nb::ndim<1>, nb::c_contig> u, int n,
+                         const nb::ndarray<T, nb::ndim<1>, nb::c_contig>& u, int n,
                          std::uint32_t cell_info)
            { self.T_apply(std::span(u.data(), u.size()), n, cell_info); })
       .def("Tt_apply_right",
            [](const FiniteElement<T>& self,
-              nb::ndarray<T, nb::ndim<1>, nb::c_contig> u, int n,
+              const nb::ndarray<T, nb::ndim<1>, nb::c_contig>& u, int n,
               std::uint32_t cell_info) {
              self.Tt_apply_right(std::span(u.data(), u.size()), n,
                                 cell_info);
            })
       .def("Tt_inv_apply", [](const FiniteElement<T>& self,
-                              nb::ndarray<T, nb::ndim<1>, nb::c_contig> u,
+                              const nb::ndarray<T, nb::ndim<1>, nb::c_contig>& u,
                               int n, std::uint32_t cell_info)
            { self.Tt_inv_apply(std::span(u.data(), u.size()), n, cell_info); })
       .def("base_transformations", [](const FiniteElement<T>& self)
@@ -358,7 +358,7 @@ void declare_float(nb::module_& m, const std::string& type)
   m.def(
       custom_name.c_str(),
       [](cell::type cell_type, const std::vector<std::size_t>& value_shape,
-         nb::ndarray<const T, nb::ndim<2>, nb::c_contig> wcoeffs,
+         const nb::ndarray<const T, nb::ndim<2>, nb::c_contig>& wcoeffs,
          std::vector<
              std::vector<nb::ndarray<const T, nb::ndim<2>, nb::c_contig>>>
              x,
@@ -420,7 +420,7 @@ void declare_float(nb::module_& m, const std::string& type)
   m.def(
       "tabulate_polynomial_set",
       [](cell::type celltype, polyset::type polytype, int d, int n,
-         nb::ndarray<const T, nb::ndim<2>, nb::c_contig> x)
+         const nb::ndarray<const T, nb::ndim<2>, nb::c_contig>& x)
       {
         mdspan_t<const T, 2> _x(x.data(), x.shape(0), x.shape(1));
         return as_nbarrayp(polyset::tabulate(celltype, polytype, d, n, _x));
